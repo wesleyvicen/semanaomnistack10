c025d4743bdac0fd1a10ff9e8a25550d3a2bf826
wesleyvicen <wesley1535@hotmail.com>
2020-01-17 01:29:49 -0300

    Instalado o Axios e fazendo requisiçao ao GitHub para pegar informaçoes do usuario que envia o user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
+const axios = require('axios');
+
 const routes = Router();
 
-routes.post('/devs', (request, response) => {
-	console.log(request.body);
+routes.post('/devs', async (request, response) => {
+	const { github_username, techs } = request.body;
+
+	const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+	let { name = login, avatar_url, bio } = apiResponse.data;
+
+	const techsArray = techs.split(',').map((tech) => tech.trim());
+
 	return response.json({ message: 'Hello world' });
 });
 
