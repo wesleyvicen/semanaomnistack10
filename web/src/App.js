415f90a1cf7dfe1d1af7b25feb308d7614da7bef
wesleyvicen <wesley1535@hotmail.com>
2020-01-21 15:04:44 -0300

    Foi explicado um pouco sobre ReactJS e os 3 principais conceitos, Componentes, Estados e propriedades

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Header from './Header';
+// Componentes: Bloco isolado de codigo HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que o componente pai passa para os filhos
+// Estado: Informações mantidas pelos componentes (Lembrar: Imutabilidade)
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ counter, setCounter ] = useState(0);
+	function incrementCounter() {
+		setCounter(counter + 1);
+	}
+	return (
+		<div>
+			<h1>Contador: {counter}</h1>
+			<button onClick={incrementCounter}>Incrementar</button>
+		</div>
+	);
 }
 
 export default App;
